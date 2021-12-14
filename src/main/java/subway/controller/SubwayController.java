ca688901bfda334c41175daab5c60a725a9decfa
Jeong Satchel <bugod96@gmail.com>
2021-12-14 21:51:24 +0900

    feat(SubwayController): 프로그램 종료 기능 구현

---
+++ src/main/java/subway/controller/SubwayController.java
@@ -0,0 +1,22 @@
+package subway.controller;
+
+import java.util.Scanner;
+
+public class SubwayController {
+
+    public SubwayController() {}
+
+    public void run() {
+        while (true) {
+            final Scanner scanner = new Scanner(System.in);
+            System.out.println("## 메인 화면\n" +
+                    "1. 역 관리\n" +
+                    "2. 노선 관리\n" +
+                    "3. 구간 관리\n" +
+                    "4. 지하철 노선도 출력\n" +
+                    "Q. 종료");
+            String userChoice = scanner.next();
+            if (userChoice.equals("Q")) break;
+        }
+    }
+}
